3cf49819cc3bf4d9390458bf7732d9217582faa2
Ota Fayz <ota_2411@mail.ru>
2021-02-04 01:47:43 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+window.addEventListener('DOMContentLoaded', () => {
+	"use strict";
+
+	// let open = document.querySelector('.hamopen'),
+	// close = document.querySelector('.hamclose'),
+	// menu = document.querySelector('.menu-mobile'),
+	// main = document.querySelector('.main'),
+	// logo = document.querySelector('.header-img'),
+	// header = document.querySelector('.header-logo'),
+	// lng = document.querySelector('.header-lng'),
+	// link = document.querySelector('.menu');	
+
+	// close.style.display = 'none';
+	// lng.style.display = 'none';
+
+	// menu.onclick = function() {
+	// 	if (open.style.display == '') {
+	// 		open.style.display = 'none';
+	// 		close.style.display = '';
+	// 		main.style.display = 'none';
+	// 		header.classList.add('justify-content-center');
+	// 		logo.style.marginTop = '94px';
+	// 		logo.style.display = 'flex';
+	// 		logo.style.justifyContent = 'center';
+	// 		lng.style.display = 'flex';
+	// 		link.style.display = 'flex';
+	// 	} else {
+	// 		open.style.display = '';
+	// 		close.style.display = 'none';
+	// 		main.style.display = '';
+	// 		logo.style.marginTop = '';
+	// 		logo.style.display = '';
+	// 		logo.style.justifyContent = '';
+	// 		logo.style.marginLeft = '';
+	// 		lng.style.display = 'none';
+	// 		link.style.display = 'none';
+	// 	}
+	// }
+
+	
+});
