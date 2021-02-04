3247236cf67422f754e0ddc52d56b7d9f87cbed2
Ota Fayz <ota_2411@mail.ru>
2021-02-04 17:45:34 +0500

    pubg tabs is added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
 	"use strict";
 
+	$(document).ready(function () {
+		let tabsItem = $('.tabs-item');
+		tabsItem.on('click', function(event){
+		  event.preventDefault();
+		  let activeContent = $(this).attr('href');
+		  $('.visible').toggleClass('visible');
+		  $(activeContent).toggleClass('visible');
+		  $('.tabs-item-active').toggleClass('tabs-item-active');
+		  $(this).toggleClass('tabs-item-active')
+		});
+	  });
+
 	// let open = document.querySelector('.hamopen'),
 	// close = document.querySelector('.hamclose'),
 	// menu = document.querySelector('.menu-mobile'),
